b6aa341f6bb6a74f273ae6184ea5bc939064b9cf
Mariia Piterova <mariia999@bk.ru>
2024-09-03 09:46:50 +0300

    check

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,18 +5,29 @@ using UnityEngine.UI;
 
 public class GameOver : MonoBehaviour
 {
+    #region Variables
+
     [SerializeField] private TMP_Text _resultLabel;
     [SerializeField] private Button _menuButton;
+
+    #endregion
+
+    #region Unity lifecycle
+
     private void Start()
     {
         _resultLabel.text = $"Correct Answers: {Game.CorrectAnswersCount}";
         _menuButton.onClick.AddListener(MenuButton);
     }
 
-   
+    #endregion
+
+    #region Private methods
 
     private void MenuButton()
     {
         SceneManager.LoadScene("StartScene");
     }
+
+    #endregion
 }
